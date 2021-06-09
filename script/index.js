6ea606e52f0c0e97a3abaaf9649b6b86a223afce
Marius <mariuslindegaard@mariuss-mbp.lan>
2021-06-09 10:23:23 +0200

    push

---
+++ script/index.js
@@ -0,0 +1,9 @@
+console.log("Hello World!")
+
+var startButton = document.getElementById("startButton")
+
+console.log(startButton)
+
+startButton.onclick = function() {
+    console.log("pressed")
+}
